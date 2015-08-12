2f8c71b9eb413081d6fbed7f534166717871a468
Adam Peterson <adam@cultivatedwit.com>
2015-08-12 12:47:10 -0700

    click check

--- js/fuckyouscript.js
+++ js/fuckyouscript.js
@@ -264,6 +264,7 @@ $(document).ready(function() {
 		$('a.random-post-link').click(function(e) {
 			e.preventDefault();
 			e.stopPropagation();
+			console.log('clicked');
 			var $linkTitle = $(this).attr('title');
 			_gaq.push(['_trackEvent', 'There\'s Fucking More link', '' + $linkTitle + ' clicked', $linkTitle]);
 			$currentPost = $(this).data('this');
