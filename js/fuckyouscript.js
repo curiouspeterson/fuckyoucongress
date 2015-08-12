e30e322ec970f6ee2d2fd902987d38cfdf719b1a
Adam Peterson <adam@cultivatedwit.com>
2015-08-12 14:26:41 -0700

    test

--- js/fuckyouscript.js
+++ js/fuckyouscript.js
@@ -288,11 +288,12 @@ $(document).ready(function() {
 		
 		if (location.pathname.substr(1)) {
 			var $id = $('.post-content[data-slug="' + location.pathname.substr(1) + '"]').attr("id");
-			var $postNum = $($id).prev();
+			var $id = $('.post-content[data-slug="' + location.pathname.substr(1) + '"]').attr("id").prev();
 			console.log($postNum);
 			var $id = "#" + $id + " a.random-post-link";
 			$($id).click();
 			$currentPost = '#post-1';
+			$nextPost = $($id).data('next');
 			$($currentPost).fadeOut("slow", function() {
 				$newHeight = $($nextPost).outerHeight();
 				$('#home-page-intro-inner').css('min-height', $newHeight);
